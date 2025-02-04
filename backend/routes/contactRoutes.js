97e63370d7f6a343c2a49b2854852a069b6c769c
Deepika7942 <151932131+Deepika7942@users.noreply.github.com>
2025-02-04 16:40:02 +0530

    Delete backend directory

--- backend/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-// backend/routes/contactRoutes.js
-const express = require('express');
-const router = express.Router();
-const contactController = require('../controllers/contactController');
-
-router.post('/', contactController.submitContactForm);
-
-module.exports = router;
